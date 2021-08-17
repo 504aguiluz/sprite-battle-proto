493406a16969114d9c6627e70ba5b6ba0bf63879
nazdak504 <aguiluz.matt@gmail.com>
2021-08-16 22:56:52 -0500

    nearly done with basic sprite animation. reactive statsand fixed positioning mostly done

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ class Sprite {
         this.maxHp = 60
         this.currentMp = 30
         this.maxMp = 30
-        this.ac = 7
+        this.ac = 5
         this.d20 = 0
         this.d8 = 0
         this.sprite1 = document.querySelector('.p1-sprite')
@@ -25,6 +25,10 @@ class Sprite {
         this.sprite2Effect = document.querySelector('#p2-effect')
         this.sprite1Points = document.querySelector('#p1-sprite-points')
         this.sprite2Points = document.querySelector('#p2-sprite-points')
+        this.p1HP = document.getElementById('p1-HP')
+        this.p2HP = document.getElementById('p2-HP')
+        this.p1MP = document.getElementById('p1-MP')
+        this.p2MP = document.getElementById('p2-MP')
         this.weapon = ['sword', 'axe', 'staff'] //to add: modifies fightDmg and spellDmg
     }
     rollD20(num = 1, rollType = ''){
@@ -37,58 +41,63 @@ class Sprite {
         console.log(`(${num})d8 ${rollType}: ${this.d8}`)
     }
 
+    updateStats(){
+        this.p1HP.innerHTML = `${player1.currentHp}/ ${player1.maxHp}`
+        this.p2HP.innerHTML = `${player2.currentHp}/ ${player2.maxHp}`
+        this.p1MP.innerHTML = `${player1.currentMp}/ ${player1.maxMp}`
+        this.p2MP.innerHTML = `${player2.currentMp}/ ${player2.maxMp}`
+    }
+
     fight(player, opponent, status = 'hit'){
-            player.rollD20(1, 'attack roll')
-            if((this.d20 >= opponent.ac) && this.d20 !== 20){
-                console.log(`${player.name} attacked ${opponent.name} for ${player.fightDmg} damage!`)
-                opponent.currentHp -= player.fightDmg
-                player.sprite1Points.innerHTML = `-${player.fightDmg}`
-                player.sprite2Points.innerHTML = `-${player.fightDmg}`
-                // console.log(`p2's HP is now:${player2.currentHp}`)
-                console.log(`p1 HP: ${player1.currentHp}/${player1.maxHp}\np1 MP: ${player1.currentMp}/${player1.maxMp}`)
-                console.log(`p2 HP: ${player2.currentHp}/${player2.maxHp}\np1 MP: ${player2.currentMp}/${player2.maxMp}`)
-                this.fightAnimation(player, status)
-            } if (this.d20 === 20){
-                opponent.currentHp -= player.fightCrit
-                console.log(`${player.name} made a critical attack on ${opponent.name} for ${player.fightCrit} damage!`)
-                console.log(`p1 HP: ${player1.currentHp}/${player1.maxHp}\np1 MP: ${player1.currentMp}/${player1.maxMp}`)
-                console.log(`p2 HP: ${player2.currentHp}/${player2.maxHp}\np1 MP: ${player2.currentMp}/${player2.maxMp}`)
-                this.fightAnimation(player, status)
-            } else if (this.d20 < opponent.ac) {
-                console.log(`${player.name} missed!`)
-                status = 'miss'
-                this.fightAnimation(player, status)
-            }
+        player.rollD20(1, 'attack roll')
+        if((this.d20 >= opponent.ac) && this.d20 !== 20){
+            console.log(`${player.name} attacked ${opponent.name} for ${player.fightDmg} damage!`)
+            opponent.currentHp -= player.fightDmg
+            player.sprite1Points.innerHTML = `-${player.fightDmg}`
+            player.sprite2Points.innerHTML = `-${player.fightDmg}`
+            this.fightAnimation(player, status)
+        } if (this.d20 === 20){
+            status = 'crit'
+            opponent.currentHp -= player.fightCrit
+            console.log(`${player.name} made a critical attack on ${opponent.name} for ${player.fightCrit} damage!`)
+            player.sprite1Points.innerHTML = `critical hit!\n-${player.fightCrit}`
+            player.sprite2Points.innerHTML = `critical hit!\n-${player.fightCrit}`
+            this.fightAnimation(player, status)
+        } else if (this.d20 < opponent.ac) {
+            console.log(`${player.name} missed!`)
+            status = 'miss'
+            this.fightAnimation(player, status)
+        }
+        this.updateStats()
     }
 
     spell(player, opponent, status = 'hit'){
-            player.rollD20(1, 'spell attack roll')
-            console.log(`d20 roll: ${this.d20}`)
-            if(this.d20 >= opponent.ac && this.d20 !== 20){
-                opponent.currentHp -= player.spellDmg
-                player.sprite1Points.innerHTML = `-${player.spellDmg}`
-                player.sprite2Points.innerHTML = `-${player.spellDmg}`
-                player.currentMp -= 5
-                // convert console logs to messages in pop-ups
-                // convert console logs to messages in pop-ups
-                console.log(`${player.name} cast spell ${opponent.name} for ${player.spellDmg} damage!`)
-                console.log(`p1 HP: ${player1.currentHp}\np1 MP: ${player1.currentMp}`)
-                console.log(`p2 HP: ${player2.currentHp}\np2 MP: ${player2.currentMp}`)
-                this.spellAnimation(player, status)
-            } if (this.d20 === 20){
-                opponent.currentHp -= player.spellCrit
-                // convert console logs to messages in pop-ups and reflect status bars
-                console.log(`${player.name} made a critical spell attack on ${opponent.name} for ${player.spellCrit} damage!`)
-                console.log(`p1 HP: ${player1.currentHp}/${player1.maxHp}\np1 MP: ${player1.currentMp}/${player1.maxMp}`)
-                console.log(`p2 HP: ${player2.currentHp}/${player2.maxHp}\np1 MP: ${player2.currentMp}/${player2.maxMp}`)
-                this.spellAnimation(player, status)
-            } else if (this.d20 < opponent.ac) {
-                // vvv comment out when finished vvv
-                console.log(`${player.name} spell missed!`)
-                status = 'miss'
-                this.spellAnimation(player, status)
-            }
+        player.rollD20(1, 'spell attack roll')
+        console.log(`d20 roll: ${this.d20}`)
+        if(this.d20 >= opponent.ac && this.d20 !== 20){
+            opponent.currentHp -= player.spellDmg
+            player.sprite1Points.innerHTML = `-${player.spellDmg}`
+            player.sprite2Points.innerHTML = `-${player.spellDmg}`
+            player.currentMp -= 5
+            // convert console logs to messages in pop-ups
+            console.log(`${player.name} cast spell ${opponent.name} for ${player.spellDmg} damage!`)
+            this.spellAnimation(player, status)
+        } if (this.d20 === 20){
+            status = 'crit'
+            opponent.currentHp -= player.spellCrit
+            player.sprite1Points.innerHTML = `critical hit!\n-${player.spellCrit}`
+            player.sprite2Points.innerHTML = `critical hit!\n-${player.spellCrit}`
+            console.log(`${player.name} made a critical spell attack on ${opponent.name} for ${player.spellCrit} damage!`)
+            player.sprite1Effect
+            this.spellAnimation(player, status)
+        } else if (this.d20 < opponent.ac) {
+            // vvv comment out when finished vvv
+            console.log(`${player.name} spell missed!`)
+            status = 'miss'
+            this.spellAnimation(player, status)
         }
+        this.updateStats()
+    }
         
     heal(player){
         player.rollD8(1)
@@ -100,6 +109,7 @@ class Sprite {
         console.log(`p1 HP: ${player1.currentHp}\np1 MP: ${player1.currentMp}`)
         console.log(`p2 HP: ${player2.currentHp}\np2 MP: ${player2.currentMp}`)
         this.healAnimation()
+        this.updateStats()
     }
 
     fightAnimation(player, status){
@@ -107,6 +117,10 @@ class Sprite {
             if (status == 'hit'){
                 this.sprite1.style.animation = "p1-attack 2s"
                 this.sprite2Points.style.animation = "p2-hp-points-down 2s"
+            } if (status == 'crit'){
+                this.sprite1.style.animation = "p1-attack 2s"
+                this.sprite2Points.style.animation = "p2-hp-points-down 2s"
+                this.sprite2Effect.style.animation = "p2-crit-struck 1s"
             } if (status == 'miss'){
                 player.sprite1Points.innerHTML = 'missed'
                 player.sprite2Points.innerHTML = 'missed'
@@ -114,11 +128,13 @@ class Sprite {
                 this.sprite2Points.style.animation = "p2-hp-points-down 2s"
             }
         } if (this.turn === 'p2') {
-            this.sprite2.style.animation = "p2-attack 2s"
-            this.sprite1Points.style.animation = "p1-hp-points-down 2s"
             if (status == 'hit'){
                 this.sprite2.style.animation = "p2-attack 2s"
                 this.sprite1Points.style.animation = "p1-hp-points-down 2s"
+            } if (status == 'crit'){
+                this.sprite2.style.animation = "p2-attack 2s"
+                this.sprite1Points.style.animation = "p1-hp-points-down 2s"
+                this.sprite1Effect.style.animation = "p1-crit-struck 1s"
             } if (status == 'miss'){
                 player.sprite2Points.innerHTML = 'missed'
                 player.sprite1Points.innerHTML = 'missed'
@@ -136,6 +152,7 @@ class Sprite {
             if (status == 'hit'){
                 this.sprite1.style.animation = "p1-spell 2s"
                 this.sprite1Effect.style.animation = "p1-spell-effect 2s ease-in"
+                this.sprite2Effect.style.animation = "p2-spell-struck 2s"
                 this.sprite2Points.style.animation = "p2-hp-points-down 2s"
                 this.struckAnimation(status)
             } if (status == 'miss') {
@@ -151,6 +168,7 @@ class Sprite {
             if (status == 'hit'){
                 this.sprite2.style.animation = "p2-spell 2s"
                 this.sprite1Points.style.animation = "p1-hp-points-down 2s"
+                this.sprite1Effect.style.animation = "p1-spell-struck 2s"
             } if (status == 'miss'){
                 player.sprite2Points.innerHTML = 'missed'
                 player.sprite1Points.innerHTML = 'missed'
@@ -159,6 +177,13 @@ class Sprite {
             }
         }
         this.struckAnimation(status)
+        if(player1.currentHp <= 0) {
+            this.sprite2.style.animation = "p1-death 500ms"
+            this.sprite1.src = "../gifs/Martial-Hero-dead.gif"
+        } if (player2.currentHp <= 0) {
+            this.sprite2.style.animation = "p2-death 2s"
+            // this.sprite2.src = "../gifs/Wizard-death.gif"
+        }
         // hipHop4.play()
     }
     
@@ -175,7 +200,7 @@ class Sprite {
             // hipHop4.play()
         }
     struckAnimation (status) {
-        if (status == 'hit'){
+        if (status == 'hit' || status == 'crit'){
             if (this.turn == 'p2') {
                 this.sprite1.style.animation = "p1-struck 500ms"
             }
@@ -218,8 +243,14 @@ function winSequence(){
 // initGame()
 
 // console log section
-console.log(`STARTING STATS\np1 HP: ${player1.currentHp}/${player1.maxHp}\np1 MP: ${player1.currentMp}/${player1.maxMp}\np2 HP: ${player2.currentHp}/${player2.maxHp}\np1 MP: ${player2.currentMp}/${player2.maxMp}`)
 
+
+// temp game conditionals
+// if(player1.currentHp <= 0) {
+//     this.sprite1.style.animation = "p1-death 2s ease-in"
+// } if (player2.currentHp <= 0) {
+//     this.sprite2.style.animation = "p2-death 2s ease-in"
+// }
 
 // event handlers
 document.getElementById('p1-fight-button').addEventListener('click', ()=>{player1.fight(player1, player2)}, ()=>{player1.fightAnimation()})
